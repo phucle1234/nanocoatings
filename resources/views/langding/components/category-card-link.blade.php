a4962d815a358cdbe1de7501e8c31a0ff6cf3945
Cristiano <coderit1372000@gmail.com>
2026-06-12 10:31:43 +0700

    thêm nút xem chi tiết cho các tab danh mục

--- resources/views/langding/components/category-card-link.blade.php
+++ resources/views/langding/components/category-card-link.blade.php
@@ -1,12 +1,11 @@
 @php
     $isYoutube = ($category->link_type ?? 'detail') === 'youtube' && !empty($category->youtube_url);
     $detailHref = route('category', ['slug' => $category->category_translation_slug ?? $category->id]);
-    $cardHref = $isYoutube ? $category->youtube_url : $detailHref;
-    $linkLabel = $isYoutube ? __('messages.watch_video') : __('messages.detail');
+    $imageHref = $isYoutube ? $category->youtube_url : $detailHref;
 @endphp
 
 <div class="product-item-img position-relative mx-auto">
-    <a href="{{ $cardHref }}" @if($isYoutube) data-fancybox @endif
+    <a href="{{ $imageHref }}" @if($isYoutube) data-fancybox @endif
         class="{{ $isYoutube ? 'category-video-thumb d-inline-block position-relative' : '' }}">
         <img src="{{ $category->category_image ?? asset('langding/imgs/product.png') }}"
             alt="{{ $category->category_name ?? 'Category' }}"
@@ -20,8 +19,8 @@
 </div>
 <div class="product-item-view mt-3">
     <a class="text-white fs-16 d-flex align-items-center justify-content-center gap-2"
-        href="{{ $cardHref }}" @if($isYoutube) data-fancybox @endif>
-        <span>{{ $linkLabel }}</span>
+        href="{{ $detailHref }}">
+        <span>{{ __('messages.detail') }}</span>
         <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2.00109 1.37635L9.28725 0.738899M9.28725 0.738899L9.92471 8.02506M9.28725 0.738899L0.738909 10.9264"
                 stroke="#ffffff" stroke-width="1.47765" stroke-linecap="round" stroke-linejoin="round" />
--- resources/views/langding/components/category-tab-content.blade.php
+++ resources/views/langding/components/category-tab-content.blade.php
@@ -1,5 +1,6 @@
 @php
     $isYoutube = ($category->link_type ?? 'detail') === 'youtube' && !empty($category->youtube_url);
+    $detailHref = route('category', ['slug' => $category->category_translation_slug ?? $category->id]);
     $hasMedia = $isYoutube || !empty($category->category_image);
     $hasText = !empty($category->category_meta_title)
         || !empty($category->category_meta_description)
@@ -50,6 +51,21 @@
                     {!! $category->category_description_html !!}
                 </div>
             @endif
+
+            <div class="text-center mt-5">
+                <a href="{{ $detailHref }}"
+                    class="btn btn-outline-secondary px-5 py-3 font-hanzel d-inline-flex align-items-center gap-2 text-white text-uppercase"
+                    style="background-color: #2CCC81; border-color: #2CCC81;">
+                    <span>{{ __('messages.detail') }}</span>
+                    <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true">
+                        <path
+                            d="M2.00109 1.37635L9.28725 0.738899M9.28725 0.738899L9.92471 8.02506M9.28725 0.738899L0.738909 10.9264"
+                            stroke="#ffffff" stroke-width="1.47765" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </a>
+            </div>
         @else
             <div class="text-center py-5">
                 <p class="fs-18 text-white">{{ __('messages.no_content') }}</p>
